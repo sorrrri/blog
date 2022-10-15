5f5f7a670524a78e6746874a2b58b5a475a7ae41
sori <sori.coralcat@developer.com>
2022-10-15 20:12:20 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    title: `Sori's blog`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "\u0000"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
